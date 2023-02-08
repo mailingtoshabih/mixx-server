2c884bdeaada6472eec346ef0fa06fcd0e67db44
Shabih Alam <mailingtoshabih@gmail.com>
2023-02-08 21:42:10 +0530

    Night Server

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use('/api/posts', postRoute);
 
 
 app.get("/", (req,res) => {
-    res.send("Kindly visit https://mixme.onrender.com");
+    res.send("Kindly visit https://bliss.onrender.com");
 })
 
 
